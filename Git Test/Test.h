8dd38c1a74a824cf0f1caf3d51344e7950d4d630
Joan Busquet <jbusquet@MacBook-Pro-de-Joan-Busquet.local>
2014-12-01 16:36:32 +0100

    added test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Joan Busquet on 1/12/14.
+//  Copyright (c) 2014 Joan Busquet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Joan Busquet on 1/12/14.
+//  Copyright (c) 2014 Joan Busquet. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
